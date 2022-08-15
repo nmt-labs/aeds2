320cb4676ca10879fddf563f6121b371e1f964ab
inathlia <nathtenaglia@hotmail.com>
2022-08-15 15:22:06 -0300

    tp1

---
+++ TP1/TP01Q01 - Palíndromo em Java/TP01Q01.java
@@ -0,0 +1,34 @@
+class TP01Q01 { 
+    public static boolean isFim(String s){
+       return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static boolean isPalindromo(String texto) {
+        boolean resp = true;
+        for(int i = 0 ; i < (texto.length())/2; i++){
+            if(texto.charAt(i) != texto.charAt(texto.length()-1-i)){
+                resp = false;
+                i = texto.length();
+            }
+        }
+        return resp;
+    }
+ 
+    public static void main (String[] args){
+       String[] entrada = new String[1000];
+       int numEntrada = 0;
+ 
+       //Leitura da entrada padrao
+       do {
+          entrada[numEntrada] = MyIO.readLine();
+       } while (isFim(entrada[numEntrada++]) == false);
+       numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+ 
+       //Para cada linha de entrada, gerando uma de saida contendo o numero de letras maiusculas da entrada
+       for(int i = 0; i < numEntrada; i++){
+          if(isPalindromo(entrada[i])) MyIO.println("SIM");
+          else MyIO.println("NAO");
+       }
+    }
+ }
+ 
