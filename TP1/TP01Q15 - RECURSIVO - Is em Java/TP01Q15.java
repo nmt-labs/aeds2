9fcbe2ab2b9b12907f56922d1c33eaaa73240835
inathlia <101769137+inathlia@users.noreply.github.com>
2022-08-25 10:25:28 -0300

    Create TP01Q15.java

---
+++ TP1/TP01Q15 - RECURSIVO - Is em Java/TP01Q15.java
@@ -0,0 +1,74 @@
+class TP01Q15 {
+    public static boolean isFim(String s){
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+     }
+
+     public static boolean soVogal(String s, int i){
+        boolean r = true;
+        String ref = "aeiouAEIOU";
+        
+        if (ref.indexOf(s.charAt(i)) == -1) r = false;
+        else r = soVogal(s, i++);
+               
+        return r;
+     }
+
+     public static boolean soConsoante(String s, int i){
+        boolean r = true;
+        String ref = "bcdfghjklmnpqrstuvwxyzBCDFGHJKLMNOPQRSTUVWXYZ";
+       
+        if (ref.indexOf(s.charAt(i)) == -1) r = false;
+        else r = soConsoante(s, i++);
+       
+        return r;
+     }
+
+     public static boolean ehInt(String s, int i){
+        boolean r = true;
+        String ref = "1234567890";
+       
+        if (ref.indexOf(s.charAt(i)) == -1) r = false;
+        else r = ehInt(s, i++);
+
+        return r;
+     }
+
+     public static boolean ehReal(String s, int i, int conta){
+        boolean r = true;
+        String ref = "1234567890.,";
+       
+        if (ref.indexOf(s.charAt(i)) == -1 || conta > 1) r = false;
+        else {
+          if(s.charAt(i) == '.' || s.charAt(i) == ',') conta++;
+          r = ehReal(s, i++, c);
+        }
+
+        return r;
+     }
+
+    public static void main(String[] args){
+        String[] entrada = new String[1000];
+        int numEntrada = 0;
+  
+        //Leitura da entrada padrao
+        do {
+           entrada[numEntrada] = MyIO.readLine();
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+  
+        //exibir resultado
+        for(int i = 0; i < numEntrada; i++){
+            if(soVogal(entrada[i], 0)) MyIO.print("SIM ");
+            else MyIO.print("NAO ");
+
+            if(soConsoante(entrada[i], 0)) MyIO.print("SIM ");
+            else MyIO.print("NAO ");
+
+            if(ehInt(entrada[i], 0)) MyIO.print("SIM ");
+            else MyIO.print("NAO ");
+
+            if(ehReal(entrada[i], 0, 0)) MyIO.print("SIM\n");
+            else MyIO.print("NAO\n");
+        }
+    }
+}
