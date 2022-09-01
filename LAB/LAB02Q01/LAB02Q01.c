e1cb2f4df83115fad7aa241ee5dac36bca2dff20
inathlia <nathtenaglia@hotmail.com>
2022-09-01 20:42:52 -0300

    lab

--- LAB/LAB02Q01/LAB02Q01.c
+++ LAB/LAB02Q01/LAB02Q01.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void intercalaFrase(char pri[1000], char seg[1000]){
-    char res[2000];
+void intercalaFrase(char pri[100], char seg[100]){
+    char res[200];
     int tam = 0, j = 0;
-    
 
     if (strlen(pri) < strlen(seg)) tam = strlen(pri);
     else tam = strlen(seg); // escolhe a menor string para ser o tamanho de referencia
 
-    for (int i = 0; i < tam; i++){ // intercala ate a menor string terminar
+    for (int i = 0; i < tam*2; i++){ // intercala ate a menor string terminar
         res[i] = pri[j];
-        res[i++] = seg[j];
+        res[++i] = seg[j];
         j++;
     }
 
     if (strlen(pri) != strlen(seg)){ // se os tamanhos forem diferentes
+        
         if (tam == strlen(pri)){ // tem que completar com o restante da segunda string
-            for(int i = tam; i < strlen(seg); i++){
-                res[i] = seg[i];
+            for(int i = (strlen(pri) * 2); i < strlen(seg) + strlen(pri); i++){
+              res[i] = seg[j];
+              j++;
             }
         }
         else { // tem que completar com o restante da segunda string
-            for(int i = tam; i < strlen(pri); i++){
-                res[i] = pri[i];
+            for(int i = (strlen(seg) * 2); i < strlen(seg) + strlen(pri); i++){
+              res[i] = pri[j];
+              j++;
             }
         }
     }
-    //erro ta aqui
-    printf("%s", res);
-
+    
+    res[strlen(pri) + strlen(seg)] = '\0';
+    printf("%s\n", res);
 }
 
 int main (){
-    char pri[1000], seg[1000], texto[1000][1000];
+    char pri[100], seg[100], texto[100][100];
     int i = 0;
 
     while (scanf("%s", pri) != EOF){ // leitura até terminar o arquivo; %s -> le ate o espaco
         scanf("%s", seg);
-        //printf("pri: %s; seg: %s\n", pri, seg);
         intercalaFrase(pri, seg);
     }
 
